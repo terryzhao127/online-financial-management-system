8534afb0f61a341c17ae11eb4e656f9969befd99
ANDI_Mckee <mckee4working@gmail.com>
2017-11-03 10:36:15 +0800

    Logout finished.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-
 from . import views
+
+import django.contrib.auth.views as auth_views
 
 
 urlpatterns = [
-    url(r'^login/$', views.custom_login, name='login')
+    url(r'^login/$', views.custom_login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'accounts/logout.html'}, name='logout')
 ]
