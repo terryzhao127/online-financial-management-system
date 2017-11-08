4b4a533e9ae6ae342272ad8e62bfa1d84b0a19a3
ANDI_Mckee <mckee4working@gmail.com>
2017-11-08 09:12:50 +0800

    Change structure of user tab and turn debug mode on.

--- errors/views.py
+++ errors/views.py
@@ -3,15 +3,9 @@ from django.shortcuts import render
 
 def error_404(request):
     # Test whether the user has logged in.
-    if request.user.is_authenticated():
-        return render(request, 'error_404_authenticated.html', status=404)
-    else:
-        return render(request, 'error_404.html', status=404)
+    return render(request, 'error_404.html', status=404)
 
 
 def error_403(request, reason=""):
     # Test whether the user has logged in.
-    if request.user.is_authenticated():
-        return render(request, 'error_csrf_authenticated.html', status=403)
-    else:
-        return render(request, 'error_csrf.html', status=403)
+    return render(request, 'error_csrf.html', status=403)
