663349a12d1d6c5db6047438fb87fedf481ba66a
ANDI_Mckee <mckee4working@gmail.com>
2017-11-11 23:57:41 +0800

    Salary frame pages added.

--- salary/urls.py
+++ salary/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.get_page, name='salary')
+    url(r'^$', views.salary, name='salary'),
+    url(r'^details/$', views.salary_details, name='salary_details'),
+    url(r'^manage/$', views.salary_manage, name='salary_manage'),
 ]
--- salary/views.py
+++ salary/views.py
@@ -3,5 +3,15 @@ from Online_Financial_Management_System.decorators import custom_login_required
 
 
 @custom_login_required
-def get_page(request, data):
+def salary(request, data):
     return render(request, 'salary/salary.html', data)
+
+
+@custom_login_required
+def salary_details(request, data):
+    return render(request, 'salary/salary_details.html', data)
+
+
+@custom_login_required
+def salary_manage(request, data):
+    return render(request, 'salary/salary_manage.html', data)
