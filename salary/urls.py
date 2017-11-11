4b161f245155d00348683db09fae72ba5c40109e
hittj <851524504@qq.com>
2017-11-12 00:01:52 +0800

    salary

--- salary/urls.py
+++ salary/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.get_page, name='salary')
+    url(r'^$', views.get_page, name='salary'),
+    url(r'upload/$', views.upload_file, name='upload'),
+    url(r'details/$', views.details, name='salary_details')
 ]
--- salary/views.py
+++ salary/views.py
@@ -5,3 +5,13 @@ from Online_Financial_Management_System.decorators import custom_login_required
 @custom_login_required
 def get_page(request, data):
     return render(request, 'salary/salary.html', data)
+
+
+@custom_login_required
+def upload_file(request, data):
+    return render(request, 'salary/upload.html', data)
+
+
+@custom_login_required
+def details(request, data):
+    return render(request, 'salary/salary_details.html', data)
