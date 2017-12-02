ec26b8a254a8a603c68bc2741a6f1ecdc2efd777
tianj <851524504@qq.com>
2017-12-02 20:40:13 +0800

    add print.js

---
+++ tables/templates/tables/print.js
@@ -0,0 +1,61 @@
+const Print = {
+  send: (params, printFrame) => {
+    // Append iframe element to document body
+    document.getElementsByTagName('body')[0].appendChild(printFrame)
+
+    // Get iframe element
+    let iframeElement = document.getElementById(params.frameId)
+
+    // Wait for iframe to load all content
+    if (params.type === 'pdf' && (Browser.isIE() || Browser.isEdge())) {
+      iframeElement.setAttribute('onload', finishPrint(iframeElement, params))
+    } else {
+      printFrame.onload = () => {
+        if (params.type === 'pdf') {
+          finishPrint(iframeElement, params)
+        } else {
+          // Get iframe element document
+          let printDocument = (iframeElement.contentWindow || iframeElement.contentDocument)
+          if (printDocument.document) printDocument = printDocument.document
+
+          // Inject printable html into iframe body
+          printDocument.body.innerHTML = params.htmlData
+
+          // If printing image, wait for it to load inside the iframe (skip firefox)
+          if (params.type === 'image') {
+            loadImageAndFinishPrint(printDocument.getElementById('printableImage'), iframeElement, params)
+          } else {
+            finishPrint(iframeElement, params)
+          }
+        }
+      }
+    }
+  }
+}
+
+function finishPrint (iframeElement, params) {
+  iframeElement.focus()
+
+  // If Edge or IE, try catch with execCommand
+  if (Browser.isEdge() || (Browser.isIE())) {
+    try {
+      iframeElement.contentWindow.document.execCommand('print', false, null)
+    } catch (e) {
+      iframeElement.contentWindow.print()
+    }
+  }
+
+  // Other browsers
+  if (!Browser.isIE() && !Browser.isEdge()) {
+    iframeElement.contentWindow.print()
+  }
+
+
+  // If we are showing a feedback message to user, remove it
+  if (params.showModal) {
+    Modal.close()
+  }
+  if (params.onLoadingEnd) {
+    params.onLoadingEnd()
+  }
+}
