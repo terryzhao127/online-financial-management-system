d922462404447244bb39111d693ce4c51d710b46
AndiMckee <mckee4working@gmail.com>
2018-03-07 14:49:05 +0800

    Update settings.py

--- Online_Financial_Management_System/settings.py
+++ Online_Financial_Management_System/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# For serving static files even in debug mode...
+# For serving static files even not in debug mode...
 if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
